6665a6378d68b8e8abce5ca8edfa653269815170
Michael Kraemer <thismjsk@gmail.com>
2017-02-15 16:57:55 -0600

    Added validation for username

--- main.py
+++ main.py
@@ -15,25 +15,47 @@
 # limitations under the License.
 #
 import webapp2
+import re
+
+contents = '''<!DOCTYPE html>
+<html>
+<head>
+    <title>User Signup</title>
+    <style type="text/css">
+    .error {color: red}
+    </style>
+</head>
+
+<body>
+
+</body>
+</html>
+
+'''
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return USER_RE.match(username)
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         username_label = "<label>Username</label>"
-        username = "<input name='username'/>"
+        username = "<input name='username' type='text' required/>"
 
         password_label = "<label>Password</label>"
-        password = "<input name='password'/>"
+        password = "<input name='password' type='password' required/>"
 
         verify_label = "<label>Verify Password</label>"
-        verify = "<input name='verify'/>"
+        verify = "<input name='verify' type='password' required/>"
 
         email_label = "<label>Email (optional)</label>"
-        email = "<input name='email'/>"
+        email = "<input name='email' type='email'/>"
 
-        submit = "<input type='submit'/>"
+        submit = "<input type='submit' action=/>"
 
 
-        form = ("<form method='post'>" +
+        form = ("<form method='get' action='/welcome'>" +
                 username_label + username + "<br>" +
                 password_label + password + "<br>" +
                 verify_label + verify + "<br>" +
@@ -43,13 +65,31 @@ class MainHandler(webapp2.RequestHandler):
         )
 
 
-        self.response.write(form)
+        header = "<h1>Signup</h1>"
 
-    def post(self):
-        self.response.write(form)
+
+
+        self.response.write(header + form)
+
+
+class Welcome(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get('username')
+        if valid_username(username):
+            self.response.write(username)
+        else:
+            self.redirect('/')
+
+
+
+        # username = valid_username(self.request.get("username"))
+        # password = valid_password(self.request.get("password"))
+        # verify = valid_verify(self.request.get("verify"))
+        # email = valid_email(self.request.get("email"))
+
 
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler), ('/welcome', Welcome)
 ], debug=True)
