9ae1cd4ee64e42bdaa8c4b8f0b29590bbf7341f0
Michael Kraemer <thismjsk@gmail.com>
2017-02-15 20:16:58 -0600

    Added validation and error messages

--- main.py
+++ main.py
@@ -17,30 +17,69 @@
 import webapp2
 import re
 
-contents = '''<!DOCTYPE html>
+page_header = '''
+<!DOCTYPE html>
 <html>
 <head>
     <title>User Signup</title>
     <style type="text/css">
-    .error {color: red}
+        .error {
+            color: red;
+
+        }
     </style>
 </head>
-
 <body>
+    <h1>Signup</h1>
 
 </body>
 </html>
 
 '''
 
+# # TODO 1
+#     #The user does not enter a username
+#     # used input type 'required'
+#
+# # TODO 2
+#     # The user's username is not valid -- for example, contains a space character. Full spec is included in the notes underneath the video.
+#
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
     return username and USER_RE.match(username)
 
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+def valid_match(password,verify):
+    if password == verify:
+        return True
+    else:
+        return False
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+def valid_email(email):
+    return email and EMAIL_RE.match(email)
+
+#
+# # TODO 3
+#     # The user's password and password-confirmation do not match
+#
+# if password != verify:
+#     error_match = "Passwords don't match"
+#
+# # TODO 4
+#     # The user provides an email, but it's not a valid email.
+#     # used input type 'email'
 
 
-class MainHandler(webapp2.RequestHandler):
+
+
+class Signup(webapp2.RequestHandler):
+
     def get(self):
+
 
         username_label = "<label>Username</label>"
         username = "<input name='username' type='text' required/>"
@@ -52,7 +91,7 @@ class MainHandler(webapp2.RequestHandler):
         verify = "<input name='verify' type='password' required/>"
 
         email_label = "<label>Email (optional)</label>"
-        email = "<input name='email' type='email'/>"
+        email = "<input name='email' type='email' />"
 
         submit = "<input type='submit' action=/>"
 
@@ -66,18 +105,38 @@ class MainHandler(webapp2.RequestHandler):
                 )
 
 
-        header = "<h1>Signup</h1>"
+        error = self.request.get('error')
+        error_element = "<p class='error'>" + error + "</p>" if error else ""
 
-        self.response.write(header + form)
+        main_content = form + error_element
+        content = page_header + main_content
+
+        self.response.write(content)
 
     def post(self):
         username = self.request.get("username")
+
         password = self.request.get("password")
+
         verify = self.request.get("verify")
+
         email = self.request.get("email")
 
-        if valid_username(username):
+        if not valid_username(username):
+            error_username = "That's not a valid username"
+            self.redirect("/?error="  + error_username)
+
+        if not valid_password(password):
+            error_password = "That's not a valid password"
+            self.redirect("/?error="  + error_password)
+
+        if not valid_match(password,verify):
+            error_match = "Passwords don't match"
+            self.redirect("/?error="  + error_match)
+
+        else:
             self.redirect('/welcome?username=' + username)
+
 
 
 
@@ -95,5 +154,6 @@ class Welcome(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler), ('/welcome', Welcome)
+    ('/', Signup),
+    ('/welcome', Welcome)
 ], debug=True)
